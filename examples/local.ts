b8fc16b1cb3bed3004569fa2825ea643a839d61c
Billy Kirk <kirk.william.c@gmail.com>
2021-12-01 14:40:34 -0600

    Remove example folders

--- examples/local.ts
+++
@@ -1,3 +0,0 @@
-import {createHTML} from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
-
-createHTML("./README.md", "./local.html");
--- examples/remote.ts
+++
@@ -1,3 +0,0 @@
-import {createHTML} from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
-
-createHTML("https://raw.githubusercontent.com/wkirk01/md2html/master/README.md", "./remote.html");
