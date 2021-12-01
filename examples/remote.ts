68768367acb8d059ba55cdb1f44ae9216cfbfc40
Billy Kirk <kirk.william.c@gmail.com>
2021-12-01 13:35:15 -0600

    Adding examples

---
+++ examples/remote.ts
@@ -0,0 +1,3 @@
+import {createHTML} from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
+
+createHTML("https://raw.githubusercontent.com/wkirk01/md2html/master/README.md", "./remote.html");
