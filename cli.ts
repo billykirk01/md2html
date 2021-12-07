843a64e00096867a0f64fc8f0ee2a877a0b1b18a
Billy Kirk <kirk.william.c@gmail.com>
2021-12-07 16:29:30 -0600

    Format using deno fmt

--- cli.ts
+++ cli.ts
@@ -1,7 +1,7 @@
-import {markdownToHTML} from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
-import {Input} from "https://deno.land/x/cliffy/prompt/input.ts";
+import { markdownToHTML } from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
+import { Input } from "https://deno.land/x/cliffy/prompt/input.ts";
 
-const input = Deno.args[0] || await Input.prompt("Markdown Location (local file or remote url)");
+const input = Deno.args[0] || await Input.prompt("Markdown Location");
 const output = Deno.args[1] || await Input.prompt("HTML Save Location");
 
 await markdownToHTML(input, output);
--- mod.ts
+++ mod.ts
@@ -1,29 +1,33 @@
-import {toFileUrl, join, isAbsolute} from "https://deno.land/std@0.117.0/path/mod.ts";
+import {
+  isAbsolute,
+  join,
+  toFileUrl,
+} from "https://deno.land/std@0.117.0/path/mod.ts";
 
 export async function markdownToHTML(input: string | URL, output: string) {
-    const markdown = await getMarkdown(input);
-    const html = await transformMarkdown(markdown);
-    await writeHTMLToFile(html, output);
+  const markdown = await getMarkdown(input);
+  const html = await transformMarkdown(markdown);
+  await writeHTMLToFile(html, output);
 }
 
 async function getMarkdown(input: string | URL) {
-    const response = await fetch(serializeURL(input));
-    return response.text();
+  const response = await fetch(serializeURL(input));
+  return response.text();
 }
 
 async function transformMarkdown(markdown: string) {
-    const response = await fetch("https://api.github.com/markdown", {
-        method: "POST",
-        headers: {
-            "Accept": "application/vnd.github.v3+json",
-        },
-        body: JSON.stringify({"text": markdown}),
-    });
-    return response.text();
+  const response = await fetch("https://api.github.com/markdown", {
+    method: "POST",
+    headers: {
+      "Accept": "application/vnd.github.v3+json",
+    },
+    body: JSON.stringify({ "text": markdown }),
+  });
+  return response.text();
 }
 
 async function writeHTMLToFile(htmlBody: string, output: string) {
-    const template = `<!DOCTYPE html>
+  const template = `<!DOCTYPE html>
     <html lang="en">
     
     <head>
@@ -35,20 +39,20 @@ async function writeHTMLToFile(htmlBody: string, output: string) {
     </head>
     
     <article class="markdown-body">
-    ${ htmlBody }
+    ${htmlBody}
     </article>
     
     </html>`;
-    await Deno.writeTextFile(output, template);
+  await Deno.writeTextFile(output, template);
 }
 
 function serializeURL(url: string | URL) {
-    if (typeof url === "string" && !url.includes("http")) {
-        if (isAbsolute(url)) {
-            return toFileUrl(url));
-        } else {
-            return toFileUrl(join(Deno.cwd(), url));
-        }
+  if (typeof url === "string" && !url.includes("http")) {
+    if (isAbsolute(url)) {
+      return toFileUrl(url);
+    } else {
+      return toFileUrl(join(Deno.cwd(), url));
     }
-    return url;
+  }
+  return url;
 }
