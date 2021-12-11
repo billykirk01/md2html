42bb1d47d84ec2ac912ea8a1dfefcb9d8c1b765e
Billy Kirk <kirk.william.c@gmail.com>
2021-12-11 08:32:45 -0600

    Update for api change

--- cli.ts
+++ cli.ts
@@ -1,7 +1,8 @@
-import { markdownToHTML } from "https://raw.githubusercontent.com/wkirk01/md2html/master/mod.ts";
-import { Input } from "https://deno.land/x/cliffy/prompt/input.ts";
+import {markdownToHTML} from "https://deno.land/x/md2html/mod.ts";
+import {Input} from "https://deno.land/x/cliffy/prompt/input.ts";
 
 const input = Deno.args[0] || await Input.prompt("Markdown Location");
 const output = Deno.args[1] || await Input.prompt("HTML Save Location");
 
-await markdownToHTML(input, output);
+const html = await markdownToHTML(input);
+await Deno.writeTextFile(output, html);
